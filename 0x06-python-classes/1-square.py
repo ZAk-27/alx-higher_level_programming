bc4208bfa86b02aaf51a4f115055332379a7ceeb
ZAk-27 <touhadi.zakaria@gmail.com>
2023-08-29 18:25:31 -0700

    update 0

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+# 0-square.py 
+"""Defining a square """
+
+
 class Square:
+    """Representing a square"""
+
     def __init__(self, size):
+        """Initializing this square class
+        Args: size - represneting the size of the square defined
+        """
+
         self.__size = size
